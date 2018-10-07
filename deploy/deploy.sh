653845ac4019283e46cb683024fb36ccca4a38d8
wilmardo <info@wilmardenouden.nl>
2018-10-07 19:20:59 +0200

    fix(deploy): Fixes deploy script and TAG_STRING

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo "Pushing ${DOCKER_NAMESPACE}/${CONTAINER_NAME}:${ANSIBLE_VERSION}"
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 docker push ${DOCKER_NAMESPACE}/${CONTAINER_NAME}
